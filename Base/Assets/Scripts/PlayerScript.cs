f740f2f249fc0e137eec5f923919e0af071edc1e
rrpanda <73320227+rrpanda@users.noreply.github.com>
2022-09-14 21:48:33 -0700

    Added player movement and camera

--- Base/Assets/Scripts/PlayerScript.cs
+++ Base/Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private CharacterController _controller;
+
+    [SerializeField] private float _playerSpeed = 5f;
+    [SerializeField] private float _rotationSpeed = 10f;
+    [SerializeField] private Camera _followCamera;
+
+    private Vector3 _playerVelocity;
+    private bool _groundedPlayer;
+
+    [SerializeField] private float _jumpHeight = 1.0f;
+    [SerializeField] private float _gravityValue = -9.81f;
+
+    private void Start()
     {
-        
+        _controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        //Prevent double jumps
+        _groundedPlayer = _controller.isGrounded;
+        if (_groundedPlayer && _playerVelocity.y < 0)
+        {
+            _playerVelocity.y = 0f;
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementInput = Quaternion.Euler(0, _followCamera.transform.eulerAngles.y, 0) *
+                                new Vector3(horizontalInput, 0, verticalInput);
+        Vector3 movementDirection = movementInput.normalized;
+
+        _controller.Move(movementDirection * (_playerSpeed * Time.deltaTime));
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion desiredRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, _rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetButtonDown("Jump") && _groundedPlayer)
+        {
+            _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -3.0f * _gravityValue);
+        }
+
+        _playerVelocity.y += _gravityValue * Time.deltaTime;
+        _controller.Move(_playerVelocity * Time.deltaTime);
     }
 }
