48374833423eb56c69dcd73712b0fa0908d57596
Sowmya <voona@usc.edu>
2022-09-15 12:58:33 -0700

    added door asset and its functionality

---
+++ Base/Assets/Scripts/DoorBehavior.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehavior : MonoBehaviour
+{
+    private Transform doorTransform;
+    bool isDoorOpen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorTransform = transform.Find("01_low"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameDetails.NumGems.Green == 1 && !isDoorOpen) {
+            print("door: all clues collected");
+            doorTransform.Rotate(0f, 0f, -90f, Space.Self);
+            isDoorOpen = true;
+        }
+
+    }
+}
