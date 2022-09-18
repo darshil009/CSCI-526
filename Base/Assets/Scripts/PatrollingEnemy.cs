f2c84517bc4ab901b26019fc7b46bda9ee3f0e9a
Priyank Jain <jainpriy@usc.edu>
2022-09-18 13:52:34 -0700

    Code for Enemy Patrolling which will stop when a player is in Visible Range of Enemy.

---
+++ Base/Assets/Scripts/PatrollingEnemy.cs
@@ -0,0 +1,67 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrollingEnemy : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    public float sightRange, attackRange;
+    public bool playerInsightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        playerInsightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+
+        if (!playerInsightRange && !playerInAttackRange) Patrolling();
+    }
+
+    private void Patrolling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //if walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+
+    private void SearchWalkPoint()
+    {
+        float randomZ = UnityEngine.Random.Range(-walkPointRange, walkPointRange);
+        float randomX = UnityEngine.Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        //walkpoint should be on the ground
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+}
