0b6754b9a32bcd9a08883be651be2ac0dde9c4d6
rrpanda <73320227+rrpanda@users.noreply.github.com>
2022-09-25 17:16:39 -0700

    camera fix

--- Base/Assets/Scripts/PlayerScript.cs
+++ Base/Assets/Scripts/PlayerScript.cs
@@ -12,12 +12,12 @@ public class PlayerScript : MonoBehaviour
 
     [SerializeField] private InventoryUI _inventoryUI;
 
-    [SerializeField] private float rotationSpeed = 10f;
+    [Range(0, 360)] [SerializeField] private float rotationSpeed;
     [SerializeField] private Camera followCamera;
     [SerializeField] private float jumpHeight = 1.0f;
     [SerializeField] private float gravityValue = -9.81f;
     [SerializeField] private float jumpButtonGracePeriod = 0.2f;
-    [SerializeField] private float  itemsRadius = 3f;
+    [SerializeField] private float itemsRadius = 3f;
     [SerializeField] private LayerMask itemMask;
 
     public static float PlayerHealth;
@@ -34,6 +34,7 @@ public class PlayerScript : MonoBehaviour
     private float maxSpeed;
     private float? lastGroundedTime;
     private float? jumpButtonPressedTime;
+    private Vector3 rotation;
 
     private bool canMove;
 
@@ -44,7 +45,7 @@ public class PlayerScript : MonoBehaviour
         _inventoryManager = new UnStackedInventoryManager();
         _inventoryUI.SetInventoryManager(_inventoryManager);
         _inventoryManager.SetInventoryUI(_inventoryUI);
-        _inventoryManager.itemAddedEvent+=OnItemPickUp;
+        _inventoryManager.itemAddedEvent += OnItemPickUp;
         _inventoryUI.OnItemRemovedFromUIEvent += OnItemDrop;
         itemInHand = null;
     }
@@ -52,18 +53,19 @@ public class PlayerScript : MonoBehaviour
 
     public Item getItemInHand()
     {
-        return itemInHand;   
+        return itemInHand;
     }
 
     public void setItemInHand(Item item)
     {
-        if(itemInHand!=null)
+        if (itemInHand != null)
         {
             _inventoryManager.AddItem(item);
         }
+
         itemInHand = item;
-        
     }
+
     private void Start()
     {
         canMove = true;
@@ -83,24 +85,23 @@ public class PlayerScript : MonoBehaviour
             playerSpeed = Mathf.Max(0.2f, (maxSpeed - (0.75f * GameDetails.currentTotalWeight)));
         MovePlayer();
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit = new RaycastHit();
-            ray.origin = Camera.main.transform.position;
-            //  Debug.DrawRay(ray.origin,ray.direction*100,Color.green);
+        RaycastHit hit = new RaycastHit();
+        ray.origin = Camera.main.transform.position;
+        //  Debug.DrawRay(ray.origin,ray.direction*100,Color.green);
 
-        if(Input.GetMouseButtonDown(0))
-        {   
-            if (Physics.Raycast(ray.origin,ray.direction, out hit,GameDetails.pickDropDistance,itemMask)){
-                Debug.Log("Hit " + hit.transform.gameObject.name + " "+hit.point);
-                if(hit.transform.GetComponent<Light>().enabled){
-                    
-                    Item item = ItemFactory.fromTag(hit.transform.tag);         
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, GameDetails.pickDropDistance, itemMask))
+            {
+                Debug.Log("Hit " + hit.transform.gameObject.name + " " + hit.point);
+                if (hit.transform.GetComponent<Light>().enabled)
+                {
+                    Item item = ItemFactory.fromTag(hit.transform.tag);
                     _inventoryManager.AddItem(item);
                     Destroy(hit.transform.gameObject);
                 }
-
+            }
         }
-        }
-
     }
 
     public async void freezePlayer(int seconds)
@@ -135,50 +136,52 @@ public class PlayerScript : MonoBehaviour
         {
             _playerVelocity.y = 0f;
         }
-
+    
         if (_groundedPlayer)
         {
             lastGroundedTime = Time.time;
         }
-
+    
         if (Input.GetButton("Jump"))
         {
             jumpButtonPressedTime = Time.time;
         }
-
-
+    
+    
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
+    
         Vector3 movementInput = Quaternion.Euler(0, followCamera.transform.eulerAngles.y, 0) *
-                                new Vector3(horizontalInput, 0, verticalInput);
+                                new Vector3(0, 0, verticalInput);
         Vector3 movementDirection = movementInput.normalized;
-
-
-
         _controller.Move(movementDirection * (playerSpeed * Time.deltaTime));
-         if (movementDirection != Vector3.zero)
-        {
-            Quaternion desiredRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-
-            transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, rotationSpeed * Time.deltaTime);
-        }
-
+        
+        rotation = new Vector3(0, horizontalInput * rotationSpeed * Time.deltaTime, 0);
+        transform.Rotate(rotation);
+        
+         // if (movementDirection != Vector3.zero)
+        // {
+        //     Quaternion desiredRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+        //
+        //     transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, rotationSpeed * Time.deltaTime);
+        // }
+    
         if (Time.time - lastGroundedTime <= jumpButtonGracePeriod)
         {
-
+    
             if (Time.time - jumpButtonPressedTime <= jumpButtonGracePeriod)
             {_playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
                 jumpButtonPressedTime = null;
                 lastGroundedTime = null;
             }
         }
-
+    
         _playerVelocity.y += gravityValue * Time.deltaTime;
         _controller.Move(_playerVelocity * Time.deltaTime);
     }
+    
 
-   void OnTriggerEnter(Collider c)
+    void OnTriggerEnter(Collider c)
     {
         if (c.CompareTag("Bullet"))
         {
@@ -186,6 +189,7 @@ public class PlayerScript : MonoBehaviour
             Destroy(c.gameObject);
         }
     }
+
     public InventoryManager getInventoryManager()
     {
         return _inventoryManager;
@@ -193,50 +197,49 @@ public class PlayerScript : MonoBehaviour
 
     private void OnItemPickUp(object sender, Item item)
     {
-            switch(item.GetItemType())
-            {
-                case Item.ItemType.Block05LB:
-                    GameDetails.currentTotalWeight+=0.5f;
-                    break;
-                case Item.ItemType.Block1LB:
-                    GameDetails.currentTotalWeight+=1f;
-                    break;
-                case Item.ItemType.Block2LB:
-                    GameDetails.currentTotalWeight+=2f;
-                    break;
-                case Item.ItemType.Block3LB:
-                    GameDetails.currentTotalWeight+=3f;
-                    break;
-                case Item.ItemType.Block5LB:
-                    GameDetails.currentTotalWeight+=5f;
-                    break;
-                default:
-                    break;
-            }
+        switch (item.GetItemType())
+        {
+            case Item.ItemType.Block05LB:
+                GameDetails.currentTotalWeight += 0.5f;
+                break;
+            case Item.ItemType.Block1LB:
+                GameDetails.currentTotalWeight += 1f;
+                break;
+            case Item.ItemType.Block2LB:
+                GameDetails.currentTotalWeight += 2f;
+                break;
+            case Item.ItemType.Block3LB:
+                GameDetails.currentTotalWeight += 3f;
+                break;
+            case Item.ItemType.Block5LB:
+                GameDetails.currentTotalWeight += 5f;
+                break;
+            default:
+                break;
+        }
     }
 
     private void OnItemDrop(object sender, Item item)
     {
-            switch(item.GetItemType())
-            {
-                case Item.ItemType.Block05LB:
-                    GameDetails.currentTotalWeight-=0.5f;
-                    break;
-                case Item.ItemType.Block1LB:
-                    GameDetails.currentTotalWeight-=1f;
-                    break;
-                case Item.ItemType.Block2LB:
-                    GameDetails.currentTotalWeight-=2f;
-                    break;
-                case Item.ItemType.Block3LB:
-                    GameDetails.currentTotalWeight-=3f;
-                    break;
-                case Item.ItemType.Block5LB:
-                    GameDetails.currentTotalWeight-=5f;
-                    break;
-                default:
-                    break;
-            }
-
+        switch (item.GetItemType())
+        {
+            case Item.ItemType.Block05LB:
+                GameDetails.currentTotalWeight -= 0.5f;
+                break;
+            case Item.ItemType.Block1LB:
+                GameDetails.currentTotalWeight -= 1f;
+                break;
+            case Item.ItemType.Block2LB:
+                GameDetails.currentTotalWeight -= 2f;
+                break;
+            case Item.ItemType.Block3LB:
+                GameDetails.currentTotalWeight -= 3f;
+                break;
+            case Item.ItemType.Block5LB:
+                GameDetails.currentTotalWeight -= 5f;
+                break;
+            default:
+                break;
+        }
     }
 }
