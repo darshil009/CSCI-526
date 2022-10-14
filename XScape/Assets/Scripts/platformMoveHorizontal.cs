02ab0311e09b711375482c4b51ad7ecbbd7a845a
param-thakker <prmthakker2@gmail.com>
2022-10-14 03:02:27 -0700

    Added platforms to move blocks

---
+++ XScape/Assets/Scripts/platformMoveHorizontal.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformMoveHorizontal : MonoBehaviour
+{
+    private Vector3 target;
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Vertical");
+        startPos = transform.position;
+        target = new Vector3(transform.position.x, transform.position.y, transform.position.z - 5.0f);
+    }
+
+    // Update is called once per frame
+
+    private void OnMouseDown()
+    {
+
+
+        Vector3 a = transform.position;
+        Vector3 b = target;
+        //Vector3 b = target.position;
+        if (b == startPos)
+        {
+            transform.DetachChildren();
+        }
+        transform.position = b;
+        //target.position = a;
+        target = a;
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.transform.parent = transform;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        collision.transform.parent = null;
+    }
+
+
+}
