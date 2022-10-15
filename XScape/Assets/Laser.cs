cc6448c30f36df7abd1eb7dacfb19f7ffcbc3d32
dsheth90 <dhrumilsheth90@gmail.com>
2022-10-15 02:21:30 -0700

    laser updated

--- XScape/Assets/Laser.cs
+++ XScape/Assets/Laser.cs
@@ -16,20 +16,21 @@ public class Laser : MonoBehaviour
 
     public void decreaseHealth(int health, string cubeName)
     {
-        if (cubeName == "MagnetBlock1")
-        {
-            object2health -= health;
-            if (object2health == 0)
-            {
-                Destroy(GameObject.FindWithTag("MagnetBlock1"), 1.0f);
-            }
-        }
-        else if (cubeName == "MagnetBlock")
+        //if (cubeName == "MagnetBlock1")
+        //{
+        //    object2health -= health;
+        //    if (object2health == 0)
+        //    {
+        //        Destroy(GameObject.FindWithTag("MagnetBlock1"));
+        //    }
+        //}
+        if (cubeName == "MagnetBlock")
         {
             object1health -= health;
             if (object1health == 0)
             {
-                Destroy(GameObject.FindWithTag("MagnetBlock"), 1.0f);
+                object1health = 100;
+                Destroy(GameObject.FindWithTag("MagnetBlock"));
             }
         }
         
@@ -41,8 +42,8 @@ public class Laser : MonoBehaviour
             lr = GetComponent<LineRenderer>();
 
             isActive = false;
-            object1health = 1000;
-            object2health = 1000;
+            object1health = 100;
+            object2health = 100;
         }
         // Update is called once per frame
     void Update()
@@ -68,11 +69,11 @@ public class Laser : MonoBehaviour
                 decreaseHealth(1, "MagnetBlock");
 
             }
-            else if (hit.transform.tag == "MagnetBlock1")
-            {
-                //Object.Destroy(gameObject, 2.0f);
-                decreaseHealth(1, "MagnetBlock1");
-            }
+            //else if (hit.transform.tag == "MagnetBlock1")
+            //{
+            //    //Object.Destroy(gameObject, 2.0f);
+            //    decreaseHealth(1, "MagnetBlock1");
+            //}
 
         else if (hit.transform.tag == "Trigger1")
         {
