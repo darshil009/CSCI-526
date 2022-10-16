1cc8356b52397b814aee666c2c7862d8a1d13f17
rrpanda <73320227+rrpanda@users.noreply.github.com>
2022-10-16 06:48:18 -0700

    Reload scene

---
+++ XScape/Assets/Scripts/Restart.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RestartGame();
+        }
+        
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
