9d7eb32c28b93ea3c9a24a2a153a2019a41521b2
rrpanda <73320227+rrpanda@users.noreply.github.com>
2022-10-20 20:32:31 -0700

    UI changes + L4 bug fix

---
+++ XScape/Assets/Scripts/screens/LevelNumber.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelNumber : MonoBehaviour
+{
+
+    public TextMeshProUGUI levelText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelText.text = "Level : " + (SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
