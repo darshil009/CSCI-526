073ad50fbbddd182f9c8fbda6b377ddad5e947c9
Sowmya <voona@usc.edu>
2022-11-30 20:47:47 -0800

    trigger prefab changes

--- XScape/Assets/Scripts/Actions/Trigger/TriggerScript.cs
+++ XScape/Assets/Scripts/Actions/Trigger/TriggerScript.cs
@@ -7,19 +7,41 @@ public class TriggerScript : MonoBehaviour
     [SerializeField] private Material triggerActiveMat;
     [SerializeField] private Material triggerInActiveMat;
 
+    [SerializeField] private Material triggerActiveLightMat;
+    [SerializeField] private Material triggerInActiveLightMat;
+
     public delegate void TriggerActive();
     public delegate void TriggerInActive();
     public event TriggerActive triggerActiveSub;
     public event TriggerInActive triggerInActiveSub;
+    
+    private Transform[] children;
+    
     AudioSource audioData;
 
     private bool isActive = false; 
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Renderer>().material = triggerInActiveMat;        
+        //GetComponent<Renderer>().material = triggerInActiveMat;   
+
+        Transform highlightTransform =  transform.Find("Highlight");
+        children = new Transform[highlightTransform.childCount];
+        for(int i = 0; i<highlightTransform.childCount; i++)
+            children[i] = highlightTransform.GetChild(i);
+
+        updateMaterial();
     }
 
+    void updateMaterial(){
+        Material triggerMat = isActive? triggerActiveMat: triggerInActiveMat;
+        Material triggerLightMat = isActive? triggerActiveLightMat: triggerInActiveLightMat;
+
+        GetComponent<Renderer>().material = triggerMat;
+        foreach(Transform child in children){
+            child.GetComponent<Renderer>().material = triggerLightMat;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
@@ -47,12 +69,14 @@ public class TriggerScript : MonoBehaviour
         audioData.Play(0);
         triggerActiveSub();
         isActive = true;
-        GetComponent<Renderer>().material = triggerActiveMat;
+        updateMaterial();
+        //GetComponent<Renderer>().material = triggerActiveMat;
     }
 
     public void deActivateTrigger(){
         triggerInActiveSub();
         isActive = false;
-        GetComponent<Renderer>().material = triggerInActiveMat;
+        updateMaterial();
+        //GetComponent<Renderer>().material = triggerInActiveMat;
     }
 }
